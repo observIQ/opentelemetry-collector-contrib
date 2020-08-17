2dbbdee46d7792408f3dcd679c335517df28304c
Dan Jaglowski <dan.jaglowski@bluemedora.com>
2020-08-17 14:39:29 -0400

    100% test coverage on conversion

--- receiver/observiqreceiver/converter_test.go
+++ receiver/observiqreceiver/converter_test.go
@@ -15,6 +15,7 @@
 package observiqreceiver
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -61,18 +62,12 @@ func TestConvertMetadata(t *testing.T) {
 	require.True(t, bod.BoolVal())
 }
 
-func TestConvertBody(t *testing.T) {
-
-	recordToBody := func(record interface{}) pdata.AttributeValue {
-		obsEntry := obsentry.New()
-		obsEntry.Record = record
-		return convert(obsEntry).ResourceLogs().At(0).InstrumentationLibraryLogs().At(0).Logs().At(0).Body()
-	}
+func TestConvertSimpleBody(t *testing.T) {
 
 	require.True(t, recordToBody(true).BoolVal())
 	require.False(t, recordToBody(false).BoolVal())
 
-	require.Equal(t, "hello", recordToBody("hello").StringVal())
+	require.Equal(t, "string", recordToBody("string").StringVal())
 	require.Equal(t, "bytes", recordToBody([]byte("bytes")).StringVal())
 
 	require.Equal(t, int64(1), recordToBody(1).IntVal())
@@ -89,6 +84,140 @@ func TestConvertBody(t *testing.T) {
 
 	require.Equal(t, float64(1), recordToBody(float32(1)).DoubleVal())
 	require.Equal(t, float64(1), recordToBody(float64(1)).DoubleVal())
+}
 
-	// TODO structured bodies
+func TestConvertMapBody(t *testing.T) {
+	structuredRecord := map[string]interface{}{
+		"true":    true,
+		"false":   false,
+		"string":  "string",
+		"bytes":   []byte("bytes"),
+		"int":     1,
+		"int8":    int8(1),
+		"int16":   int16(1),
+		"int32":   int32(1),
+		"int64":   int64(1),
+		"uint":    uint(1),
+		"uint8":   uint8(1),
+		"uint16":  uint16(1),
+		"uint32":  uint32(1),
+		"uint64":  uint64(1),
+		"float32": float32(1),
+		"float64": float64(1),
+	}
+
+	result := recordToBody(structuredRecord).MapVal()
+
+	v, _ := result.Get("true")
+	require.True(t, v.BoolVal())
+	v, _ = result.Get("false")
+	require.False(t, v.BoolVal())
+
+	for _, k := range []string{"string", "bytes"} {
+		v, _ = result.Get(k)
+		require.Equal(t, k, v.StringVal())
+	}
+	for _, k := range []string{"int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64"} {
+		v, _ = result.Get(k)
+		require.Equal(t, int64(1), v.IntVal())
+	}
+	for _, k := range []string{"float32", "float64"} {
+		v, _ = result.Get(k)
+		require.Equal(t, float64(1), v.DoubleVal())
+	}
+}
+
+func TestConvertArrayBody(t *testing.T) {
+	result := recordToBody([]interface{}{0, 1}).MapVal()
+
+	v, _ := result.Get("0")
+	require.Equal(t, int64(0), v.IntVal())
+
+	v, _ = result.Get("1")
+	require.Equal(t, int64(1), v.IntVal())
+}
+
+func TestConvertUnknownBody(t *testing.T) {
+	unknownType := map[string]int{"0": 0, "1": 1}
+	require.Equal(t, fmt.Sprintf("%v", unknownType), recordToBody(unknownType).StringVal())
+}
+
+func TestConvertNestedMapBody(t *testing.T) {
+
+	unknownType := map[string]int{"0": 0, "1": 1}
+
+	structuredRecord := map[string]interface{}{
+		"array":   []interface{}{0, 1},
+		"map":     map[string]interface{}{"0": 0, "1": "one"},
+		"unknown": unknownType,
+	}
+
+	result := recordToBody(structuredRecord).MapVal()
+
+	arrayAttVal, _ := result.Get("array")
+	a := arrayAttVal.MapVal()
+	v, _ := a.Get("0")
+	require.Equal(t, int64(0), v.IntVal())
+	v, _ = a.Get("1")
+	require.Equal(t, int64(1), v.IntVal())
+
+	mapAttVal, _ := result.Get("map")
+	m := mapAttVal.MapVal()
+	v, _ = m.Get("0")
+	require.Equal(t, int64(0), v.IntVal())
+	v, _ = m.Get("1")
+	require.Equal(t, "one", v.StringVal())
+
+	unknownAttVal, _ := result.Get("unknown")
+	require.Equal(t, fmt.Sprintf("%v", unknownType), unknownAttVal.StringVal())
+}
+
+func recordToBody(record interface{}) pdata.AttributeValue {
+	entry := obsentry.New()
+	entry.Record = record
+	return convertAndDrill(entry).Body()
+}
+
+func convertAndDrill(entry *obsentry.Entry) pdata.LogRecord {
+	return convert(entry).ResourceLogs().At(0).InstrumentationLibraryLogs().At(0).Logs().At(0)
+}
+
+func TestConvertSeverity(t *testing.T) {
+	cases := []struct {
+		severity       obsentry.Severity
+		expectedNumber pdata.SeverityNumber
+		expectedText   string
+	}{
+		{obsentry.Default, pdata.SeverityNumberUNDEFINED, "Undefined"},
+		{5, pdata.SeverityNumberTRACE, "Trace"},
+		{obsentry.Trace, pdata.SeverityNumberTRACE2, "Trace"},
+		{15, pdata.SeverityNumberTRACE3, "Trace"},
+		{obsentry.Debug, pdata.SeverityNumberDEBUG, "Debug"},
+		{25, pdata.SeverityNumberDEBUG2, "Debug"},
+		{obsentry.Info, pdata.SeverityNumberINFO, "Info"},
+		{35, pdata.SeverityNumberINFO2, "Info"},
+		{obsentry.Notice, pdata.SeverityNumberINFO3, "Info"},
+		{45, pdata.SeverityNumberINFO3, "Info"},
+		{obsentry.Warning, pdata.SeverityNumberINFO4, "Info"},
+		{55, pdata.SeverityNumberINFO4, "Info"},
+		{obsentry.Error, pdata.SeverityNumberERROR, "Error"},
+		{65, pdata.SeverityNumberERROR2, "Error"},
+		{obsentry.Critical, pdata.SeverityNumberERROR2, "Error"},
+		{75, pdata.SeverityNumberERROR3, "Error"},
+		{obsentry.Alert, pdata.SeverityNumberERROR3, "Error"},
+		{85, pdata.SeverityNumberERROR4, "Error"},
+		{obsentry.Emergency, pdata.SeverityNumberFATAL, "Error"},
+		{95, pdata.SeverityNumberFATAL2, "Fatal"},
+		{obsentry.Catastrophe, pdata.SeverityNumberFATAL4, "Fatal"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%v", tc.severity), func(t *testing.T) {
+			entry := obsentry.New()
+			entry.Severity = tc.severity
+			log := convertAndDrill(entry)
+			require.Equal(t, tc.expectedNumber, log.SeverityNumber())
+			require.Equal(t, tc.expectedText, log.SeverityText())
+		})
+	}
 }
