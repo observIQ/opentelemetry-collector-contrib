c4bd0ad5bb47530861b757ff0a6e9c91ef682876
Jonathan Wamsley <jonnywamsley@gmail.com>
2021-07-08 15:13:42 -0400

    updated scraper test

--- receiver/mysqlreceiver/scraper_test.go
+++ receiver/mysqlreceiver/scraper_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/mysqlreceiver/internal/metadata"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/model/pdata"
 	"go.uber.org/zap"
 )
 
@@ -23,6 +24,7 @@ func TestScraper(t *testing.T) {
 
 	// err := sc.start(context.Background(), componenttest.NewNopHost())
 	// require.NoError(t, err)
+
 	rms, err := sc.scrape(context.Background())
 	require.Nil(t, err)
 
@@ -35,275 +37,100 @@ func TestScraper(t *testing.T) {
 	ilm := ilms.At(0)
 	ms := ilm.Metrics()
 
-	require.Equal(t, 14, ms.Len())
+	require.Equal(t, 67, ms.Len())
+	require.Equal(t, 14, len(metadata.M.Names()))
+
+	floatMetrics := map[string]float64{}
+	intMetrics := map[string]int64{}
 
 	for i := 0; i < ms.Len(); i++ {
 		m := ms.At(i)
-		switch m.Name() {
-		case metadata.M.MysqlBufferPoolPages.Name():
-			dps := m.DoubleGauge().DataPoints()
-			require.Equal(t, 6, dps.Len())
-			bufferPoolPagesMetrics := map[string]float64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.BufferPoolPagesState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				bufferPoolPagesMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 6, len(bufferPoolPagesMetrics))
-			require.Equal(t, map[string]float64{
-				"mysql.buffer_pool_pages state:data":    981,
-				"mysql.buffer_pool_pages state:dirty":   0,
-				"mysql.buffer_pool_pages state:flushed": 168,
-				"mysql.buffer_pool_pages state:free":    7207,
-				"mysql.buffer_pool_pages state:misc":    4,
-				"mysql.buffer_pool_pages state:total":   8192,
-			}, bufferPoolPagesMetrics)
-		case metadata.M.MysqlBufferPoolOperations.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 7, dps.Len())
-			bufferPoolOperationsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.BufferPoolOperationsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				bufferPoolOperationsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 7, len(bufferPoolOperationsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.buffer_pool_operations state:read_ahead":         0,
-				"mysql.buffer_pool_operations state:read_ahead_evicted": 0,
-				"mysql.buffer_pool_operations state:read_ahead_rnd":     0,
-				"mysql.buffer_pool_operations state:read_requests":      14837,
-				"mysql.buffer_pool_operations state:reads":              838,
-				"mysql.buffer_pool_operations state:wait_free":          0,
-				"mysql.buffer_pool_operations state:write_requests":     1669,
-			}, bufferPoolOperationsMetrics)
-		case metadata.M.MysqlBufferPoolSize.Name():
-			dps := m.DoubleGauge().DataPoints()
-			require.Equal(t, 3, dps.Len())
-			bufferPoolSizeMetrics := map[string]float64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.BufferPoolSizeState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				bufferPoolSizeMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 3, len(bufferPoolSizeMetrics))
-			require.Equal(t, map[string]float64{
-				"mysql.buffer_pool_size state:data":  16072704,
-				"mysql.buffer_pool_size state:dirty": 0,
-				"mysql.buffer_pool_size state:size":  134217728,
-			}, bufferPoolSizeMetrics)
-		case metadata.M.MysqlCommands.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 6, dps.Len())
-			commandsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.CommandState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				commandsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 6, len(commandsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.commands state:close":          0,
-				"mysql.commands state:execute":        0,
-				"mysql.commands state:fetch":          0,
-				"mysql.commands state:prepare":        0,
-				"mysql.commands state:reset":          0,
-				"mysql.commands state:send_long_data": 0,
-			}, commandsMetrics)
-		case metadata.M.MysqlHandlers.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 18, dps.Len())
-			handlersMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.HandlerState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				handlersMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 18, len(handlersMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.handlers state:commit":             564,
-				"mysql.handlers state:delete":             0,
-				"mysql.handlers state:discover":           0,
-				"mysql.handlers state:lock":               7127,
-				"mysql.handlers state:mrr_init":           0,
-				"mysql.handlers state:prepare":            0,
-				"mysql.handlers state:read_first":         52,
-				"mysql.handlers state:read_key":           1680,
-				"mysql.handlers state:read_last":          0,
-				"mysql.handlers state:read_next":          3960,
-				"mysql.handlers state:read_prev":          0,
-				"mysql.handlers state:read_rnd":           0,
-				"mysql.handlers state:read_rnd_next":      505063,
-				"mysql.handlers state:rollback":           0,
-				"mysql.handlers state:savepoint":          0,
-				"mysql.handlers state:savepoint_rollback": 0,
-				"mysql.handlers state:update":             315,
-				"mysql.handlers state:write":              256734,
-			}, handlersMetrics)
-		case metadata.M.MysqlDoubleWrites.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 2, dps.Len())
-			doubleWritesMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.DoubleWritesState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				doubleWritesMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 2, len(doubleWritesMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.double_writes state:writes":  8,
-				"mysql.double_writes state:written": 27,
-			}, doubleWritesMetrics)
-		case metadata.M.MysqlLogOperations.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 3, dps.Len())
-			logOperationsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.LogOperationsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				logOperationsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 3, len(logOperationsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.log_operations state:requests": 646,
-				"mysql.log_operations state:waits":    0,
-				"mysql.log_operations state:writes":   14,
-			}, logOperationsMetrics)
-		case metadata.M.MysqlOperations.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 3, dps.Len())
-			operationsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.OperationsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				operationsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 3, len(operationsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.operations state:fsyncs": 46,
-				"mysql.operations state:reads":  860,
-				"mysql.operations state:writes": 215,
-			}, operationsMetrics)
-		case metadata.M.MysqlPageOperations.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 3, dps.Len())
-			pageOperationsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.PageOperationsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				pageOperationsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 3, len(pageOperationsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.page_operations state:created": 144,
-				"mysql.page_operations state:read":    837,
-				"mysql.page_operations state:written": 168,
-			}, pageOperationsMetrics)
-		case metadata.M.MysqlRowLocks.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 2, dps.Len())
-			rowLocksMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.RowLocksState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				rowLocksMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 2, len(rowLocksMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.row_locks state:time":  0,
-				"mysql.row_locks state:waits": 0,
-			}, rowLocksMetrics)
-		case metadata.M.MysqlRowOperations.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 4, dps.Len())
-			rowOperationsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.RowOperationsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				rowOperationsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 4, len(rowOperationsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.row_operations state:deleted":  0,
-				"mysql.row_operations state:inserted": 0,
-				"mysql.row_operations state:read":     0,
-				"mysql.row_operations state:updated":  0,
-			}, rowOperationsMetrics)
-		case metadata.M.MysqlLocks.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 2, dps.Len())
-			locksMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.LocksState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				locksMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 2, len(locksMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.locks state:immediate": 521,
-				"mysql.locks state:waited":    0,
-			}, locksMetrics)
-		case metadata.M.MysqlSorts.Name():
-			dps := m.IntSum().DataPoints()
-			require.True(t, m.IntSum().IsMonotonic())
-			require.Equal(t, 4, dps.Len())
-			sortsMetrics := map[string]int64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.SortsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				sortsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 4, len(sortsMetrics))
-			require.Equal(t, map[string]int64{
-				"mysql.sorts state:merge_passes": 0,
-				"mysql.sorts state:range":        0,
-				"mysql.sorts state:rows":         0,
-				"mysql.sorts state:scan":         0,
-			}, sortsMetrics)
-		case metadata.M.MysqlThreads.Name():
-			dps := m.DoubleGauge().DataPoints()
-			require.Equal(t, 4, dps.Len())
-			threadsMetrics := map[string]float64{}
-			for j := 0; j < dps.Len(); j++ {
-				dp := dps.At(j)
-				state, _ := dp.LabelsMap().Get(metadata.L.ThreadsState)
-				label := fmt.Sprintf("%s state:%s", m.Name(), state)
-				threadsMetrics[label] = dp.Value()
-			}
-			require.Equal(t, 4, len(threadsMetrics))
-			require.Equal(t, map[string]float64{
-				"mysql.threads state:cached":    0,
-				"mysql.threads state:connected": 1,
-				"mysql.threads state:created":   1,
-				"mysql.threads state:running":   2,
-			}, threadsMetrics)
+
+		fmt.Println(m.DataType())
+		switch m.DataType() {
+		case pdata.MetricDataTypeDoubleGauge:
+			fmt.Println(m.Name(), m.DoubleGauge().DataPoints().At(0).Value())
+			floatMetrics[m.Name()] = m.DoubleGauge().DataPoints().At(0).Value()
+		case pdata.MetricDataTypeIntSum:
+			fmt.Println(m.Name(), m.IntSum().DataPoints().At(0).Value())
+			intMetrics[m.Name()] = m.IntSum().DataPoints().At(0).Value()
 		default:
 			require.Nil(t, m.Name(), fmt.Sprintf("metrics %s not expected", m.Name()))
 		}
 	}
+
+	require.Equal(t, 13, len(floatMetrics))
+	require.Equal(t, map[string]float64{
+		"Innodb_buffer_pool_bytes_data":    1.6072704e+07,
+		"Innodb_buffer_pool_bytes_dirty":   0,
+		"Innodb_buffer_pool_pages_data":    981,
+		"Innodb_buffer_pool_pages_dirty":   0,
+		"Innodb_buffer_pool_pages_flushed": 168,
+		"Innodb_buffer_pool_pages_free":    7207,
+		"Innodb_buffer_pool_pages_misc":    4,
+		"Innodb_buffer_pool_pages_total":   8192,
+		"Threads_cached":                   0,
+		"Threads_connected":                1,
+		"Threads_created":                  1,
+		"Threads_running":                  2,
+		"buffer_pool_size":                 1.34217728e+08}, floatMetrics)
+
+	require.Equal(t, map[string]int64{
+		"Com_stmt_close":                        0,
+		"Com_stmt_execute":                      0,
+		"Com_stmt_fetch":                        0,
+		"Com_stmt_prepare":                      0,
+		"Com_stmt_reset":                        0,
+		"Com_stmt_send_long_data":               0,
+		"Handler_commit":                        564,
+		"Handler_delete":                        0,
+		"Handler_discover":                      0,
+		"Handler_external_lock":                 7127,
+		"Handler_mrr_init":                      0,
+		"Handler_prepare":                       0,
+		"Handler_read_first":                    52,
+		"Handler_read_key":                      1680,
+		"Handler_read_last":                     0,
+		"Handler_read_next":                     3960,
+		"Handler_read_prev":                     0,
+		"Handler_read_rnd":                      0,
+		"Handler_read_rnd_next":                 505063,
+		"Handler_rollback":                      0,
+		"Handler_savepoint":                     0,
+		"Handler_savepoint_rollback":            0,
+		"Handler_update":                        315,
+		"Handler_write":                         256734,
+		"Innodb_buffer_pool_read_ahead":         0,
+		"Innodb_buffer_pool_read_ahead_evicted": 0,
+		"Innodb_buffer_pool_read_ahead_rnd":     0,
+		"Innodb_buffer_pool_read_requests":      14837,
+		"Innodb_buffer_pool_reads":              838,
+		"Innodb_buffer_pool_wait_free":          0,
+		"Innodb_buffer_pool_write_requests":     1669,
+		"Innodb_data_fsyncs":                    46,
+		"Innodb_data_reads":                     860,
+		"Innodb_data_writes":                    215,
+		"Innodb_dblwr_pages_written":            27,
+		"Innodb_dblwr_writes":                   8,
+		"Innodb_log_waits":                      0,
+		"Innodb_log_write_requests":             646,
+		"Innodb_log_writes":                     14,
+		"Innodb_pages_created":                  144,
+		"Innodb_pages_read":                     837,
+		"Innodb_pages_written":                  168,
+		"Innodb_row_lock_time":                  0,
+		"Innodb_row_lock_waits":                 0,
+		"Innodb_rows_deleted":                   0,
+		"Innodb_rows_inserted":                  0,
+		"Innodb_rows_read":                      0,
+		"Innodb_rows_updated":                   0,
+		"Sort_merge_passes":                     0,
+		"Sort_range":                            0,
+		"Sort_rows":                             0,
+		"Sort_scan":                             0,
+		"Table_locks_immediate":                 521,
+		"Table_locks_waited":                    0,
+	}, intMetrics)
 }
 
 func TestScrapeErrorBadConfig(t *testing.T) {
