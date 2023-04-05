b20f7efad5cbd037616610a62557efdf79c2f973
Caleb Hurshman <caleb@hurshman.com>
2023-04-05 09:39:50 -0400

    scraper-test

--- receiver/apachepulsarreceiver/scraper_test.go
+++ receiver/apachepulsarreceiver/scraper_test.go
@@ -29,7 +29,7 @@ func TestScraper(t *testing.T) {
 	require.NoError(t, err)
 	// validate metrics are getting emitted
 	expectedFile := filepath.Join("testdata", "scraper", "expected.yaml")
-	golden.WriteMetrics(t, expectedFile, actualMetrics)
+	// golden.WriteMetrics(t, expectedFile, actualMetrics)
 	expectedMetrics, err := golden.ReadMetrics(expectedFile)
 	require.NoError(t, err)
 
@@ -38,31 +38,6 @@ func TestScraper(t *testing.T) {
 }
 
 func newMockClient(t *testing.T) client {
-	// mockTenant := &mocks.MockTenants{}
-	// mockTenant.On("List").Return([]string{"public", "pulsar"}, nil)
-	// mockClient := &mocks.MockClient{}
-	// mockClient.On("Tenants").Return(mockTenant)
-
-	// returnedNamespaces := []string{"Hello", "World"}
-	// mockNamespace := &mocks.MockNamespaces{}
-	// mockNamespace.On("GetNamespaces", mock.Anything).Return(returnedNamespaces, nil)
-	// mockNamespace.On("GetTopics", mock.Anything).Return([]string{"my-topic"}, nil)
-	// mockClient.On("Namespaces").Return(mockNamespace)
-
-	// mockTopic := &mocks.MockTopics{}
-	// mockTopic.On("GetStats", mock.Anything).Return(utils.TopicStats{
-	// 	MsgRateIn:           0.0,
-	// 	MsgRateOut:          0.0,
-	// 	MsgThroughputIn:     0.0,
-	// 	MsgThroughputOut:    0.0,
-	// 	AverageMsgSize:      0.0,
-	// 	StorageSize:         504,
-	// 	Publishers:          []utils.PublisherStats{},
-	// 	Subscriptions:       map[string]utils.SubscriptionStats{},
-	// 	Replication:         map[string]utils.ReplicatorStats{},
-	// 	DeDuplicationStatus: "Disabled",
-	// }, nil)
-	// mockClient.On("Topics").Return(mockTopic)
 
 	mockReceiverClient := &mocks.MockReceiverClient{}
 
