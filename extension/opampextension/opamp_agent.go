20c7f049d00422ad209bc46152ddf083c8262ca4
Brandon Johnson <brandon.johnson@observiq.com>
2024-04-09 15:21:30 -0400

    ensure extension is a valid registry

--- extension/opampextension/opamp_agent.go
+++ extension/opampextension/opamp_agent.go
@@ -45,6 +45,8 @@ type opampAgent struct {
 	customCapabilityRegistry *customCapabilityRegistry
 }
 
+var _ CustomCapabilityRegistry = (*opampAgent)(nil)
+
 func (o *opampAgent) Start(_ context.Context, _ component.Host) error {
 	header := http.Header{}
 	for k, v := range o.cfg.Server.GetHeaders() {
