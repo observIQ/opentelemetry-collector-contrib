f5197675a32188538b8039c34802bf41f8f39a8e
Brandon Johnson <brandon.johnson@observiq.com>
2024-08-27 22:36:48 -0400

    add benchmark for basic batch set for log

--- processor/transformprocessor/processor_test.go
+++ processor/transformprocessor/processor_test.go
@@ -5,13 +5,17 @@ package transformprocessor
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/processor/processortest"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/plogtest"
@@ -143,5 +147,40 @@ func BenchmarkLogsWithFlatten(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		assert.NoError(b, p.ConsumeLogs(context.Background(), input))
+	}
+}
+
+func BenchmarkSetLogRecords(b *testing.B) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	oCfg := cfg.(*Config)
+	oCfg.LogStatements = []common.ContextStatements{
+		{
+			Context: "log",
+			Statements: []string{
+				`set(attributes["my_attribute"], "my_value")`,
+			},
+		},
+	}
+	sink := new(consumertest.LogsSink)
+
+	set := processortest.NewNopSettings()
+	spanRecorder := tracetest.NewSpanRecorder()
+	set.TelemetrySettings.TracerProvider = trace.NewTracerProvider(trace.WithSpanProcessor(spanRecorder))
+
+	p, err := factory.CreateLogsProcessor(context.Background(), set, oCfg, sink)
+	require.NoError(b, err)
+
+	logs := plog.NewLogs()
+	scopeLogs := logs.ResourceLogs().AppendEmpty().ScopeLogs().AppendEmpty()
+
+	for i := range 200 {
+		lr := scopeLogs.LogRecords().AppendEmpty()
+		lr.Body().SetStr(fmt.Sprintf("Log body: %d", i))
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		assert.NoError(b, p.ConsumeLogs(context.Background(), logs))
 	}
 }
